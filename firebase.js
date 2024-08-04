a21edca9f7dc9c9bb4210a7d535410d110b168b2
Vivian Zhou <vivianzhou1288@gmail.com>
2024-08-04 15:13:24 -0400

    Removed some comments

--- firebase.js
+++ firebase.js
@@ -7,15 +7,7 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDsnwltfaVLnga8Ts-QthvZjekEOvY8rEw",
-//   authDomain: "pantry-app-1de27.firebaseapp.com",
-//   projectId: "pantry-app-1de27",
-//   storageBucket: "pantry-app-1de27.appspot.com",
-//   messagingSenderId: "534227855949",
-//   appId: "1:534227855949:web:5ea25244b9888a8b5e0a04",
-//   measurementId: "G-7WN2F4VG8S",
-// };
+
 const firebaseConfig = {
   apiKey: process.env.apiKey,
   authDomain: process.env.authDomain,
